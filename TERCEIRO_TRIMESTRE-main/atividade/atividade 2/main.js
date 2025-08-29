5bc1e0a480015848a5d3dc37ecbb4ed4e00eb386
douglaslindo <douglas.tomaz.oliveira@escola.pr.gov.br>
2025-08-29 09:28:17 -0300

    Delete TERCEIRO_TRIMESTRE-main directory

--- TERCEIRO_TRIMESTRE-main/atividade/atividade 2/main.js
+++
@@ -1,9 +0,0 @@
-// cria a referência ao form e ao elemento h3 (onde será exibida a respsota)
-const frm = document.querySelector("form")
-const resp = document.querySelector("h3")
-
-frm.addEventListener("submit", (e) => {
-    const nome = frm.inNome.value
-    resp.innerText = `Olá, ${nome}`
-    e.preventDefault()
-})
